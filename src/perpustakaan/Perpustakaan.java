e7d924aea0f9da6a845b95af7d6b92ce05a28d16
Dedy-PC <Dedy-PC@DEDY-PC>
2018-12-16 19:54:05 +0700

    dedy

---
+++ src/perpustakaan/Perpustakaan.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perpustakaan;
+import java.util.Scanner;
+import java.util.Arrays;
+
+/**
+ *
+ * @author Dedy-PC
+ */
+public class Perpustakaan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner a = new Scanner(System. in );
+int nim, jenis, harga_sewa;
+int total_harga=0;
+String namaMHS;
+System.out.println ("===========================================================");
+System.out.println ("INPUT DATA PEMINJAM");
+System.out.println ("===========================================================");
+System.out.print   ("Masukkan Nama       = ");
+namaMHS = a.nextLine();
+System.out.print   ("Masukkan Nim        = ");
+nim = a.nextInt();
+System.out.print   ("Masukan Jumlah Buku = ");
+jenis = a.nextInt();
+System.out.println ("==========================================================");
+
+int jenis_arr[] = new int[jenis];
+String judul_arr[] = new String[jenis];
+for (int b = 0; b < jenis; b++) {
+System.out.print("Masukan Judul Buku : ");
+judul_arr[b] = a.next();
+System.out.print("Jenis Buku : 1. Komputer | 2. Non Komputer \nMasukan pilihan buku : ");
+jenis_arr[b] = a.nextInt();
+if(jenis_arr[b]==1){
+harga_sewa = 5000;
+
+total_harga = total_harga+harga_sewa;
+}else if(jenis_arr[b]==2){
+harga_sewa = 3000;
+
+total_harga = total_harga+harga_sewa;
+}
+}
+System.out.println("");
+System.out.println ("==========================================================");
+System.out.println ("DATA PEMINJAM");
+System.out.println ("==========================================================");
+System.out.println ("Nama Mahasiswa               = "+namaMHS);
+System.out.println ("Nim Mahasiswa                = "+nim);
+System.out.println ("Total Biaya Pinjam           = "+total_harga);
+System.out.println ("==========================================================");
+System.out.println ("DAFTAR BUKU YANG DIPINJAM");
+System.out.println ("==========================================================");
+for (int b = 0;b<jenis;b++){
+System.out.println ("Judul Buku Ke : " + b +": " + judul_arr [b]);
+    }
+    }  
+}
